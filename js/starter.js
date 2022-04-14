89ad35d5ed849e3b33eb006ee77eb35abfd48773
Jacob Wagner <jtwagner@stanford.edu>
2022-04-13 17:40:48 -0700

    Under Construction updates

---
+++ js/starter.js
@@ -0,0 +1,140 @@
+$(document).ready(function () {
+    $(document).matrixTicker();
+    $("#restart").on("click", function () {
+      $(document).matrixTicker();
+    });
+  });
+  
+  $.fn.extend({
+    matrixTicker: function () {
+      setTimeout(function () {
+        $("#prompt").html("U");
+      }, 0);
+      setTimeout(function () {
+        $("#prompt").html("Un");
+      }, 300);
+      setTimeout(function () {
+        $("#prompt").html("Und");
+      }, 600);
+      setTimeout(function () {
+        $("#prompt").html("Unde");
+      }, 900);
+      setTimeout(function () {
+        $("#prompt").html("Under");
+      }, 1200);
+      setTimeout(function () {
+        $("#prompt").html("Under ");
+      }, 1500);
+      setTimeout(function () {
+        $("#prompt").html("Under C");
+      }, 1800);
+      setTimeout(function () {
+        $("#prompt").html("Under Co");
+      }, 2100);
+      setTimeout(function () {
+        $("#prompt").html("Under Con");
+      }, 2400);
+      setTimeout(function () {
+        $("#prompt").html("Under Cons");
+      }, 2700);
+      setTimeout(function () {
+        $("#prompt").html("Under Const");
+      }, 3000);
+      setTimeout(function () {
+        $("#prompt").html("Under Constr");
+      }, 3300);
+      setTimeout(function () {
+        $("#prompt").html("Under Constru");
+      }, 3600);
+      setTimeout(function () {
+        $("#prompt").html("Under Construc");
+      }, 3900);
+      setTimeout(function () {
+        $("#prompt").html("Under Construct");
+      }, 4200);
+      setTimeout(function () {
+        $("#prompt").html("Under Constructi");
+      }, 4500);
+      setTimeout(function () {
+        $("#prompt").html("Under Constructio");
+      }, 4800);
+      setTimeout(function () {
+        $("#prompt").html("Under Construction");
+      }, 5100);
+      setTimeout(function () {
+        $("#prompt").html("Under Construction.");
+      }, 5400);
+      setTimeout(function () {
+        $("#prompt").html("Under Construction..");
+      }, 5700);
+      setTimeout(function () {
+        $("#prompt").html("Under Construction...");
+      }, 6000);
+  
+      //
+      setTimeout(function () {
+        $("#prompt").html("S");
+      }, 8000);
+      setTimeout(function () {
+        $("#prompt").html("Se");
+      }, 8300);
+      setTimeout(function () {
+        $("#prompt").html("See");
+      }, 8600);
+      setTimeout(function () {
+        $("#prompt").html("See ");
+      }, 8900);
+      setTimeout(function () {
+        $("#prompt").html("See Y");
+      }, 9200);
+      setTimeout(function () {
+        $("#prompt").html("See Yo");
+      }, 9500);
+      setTimeout(function () {
+        $("#prompt").html("See You");
+      }, 9800);
+      setTimeout(function () {
+        $("#prompt").html("See You A");
+      }, 10100);
+      setTimeout(function () {
+        $("#prompt").html("See You Ag");
+      }, 10400);
+      setTimeout(function () {
+        $("#prompt").html("See You Aga");
+      }, 10700);
+      setTimeout(function () {
+        $("#prompt").html("See You Agai");
+      }, 11000);
+      setTimeout(function () {
+        $("#prompt").html("See You Again");
+      }, 11300);
+      setTimeout(function () {
+        $("#prompt").html("See You Again ");
+      }, 11600);
+      setTimeout(function () {
+        $("#prompt").html("See You Again S");
+      }, 11900);
+      setTimeout(function () {
+        $("#prompt").html("See You Again So");
+      }, 12200);
+      setTimeout(function () {
+        $("#prompt").html("See You Again Soo");
+      }, 12500);
+      setTimeout(function () {
+        $("#prompt").html("See You Again Soon");
+      }, 12800);
+      setTimeout(function () {
+        $("#prompt").html("See You Again Soon");
+      }, 13100);
+      setTimeout(function () {
+        $("#prompt").html("See You Again Soon.");
+      }, 13400);
+      setTimeout(function () {
+        $("#prompt").html("See You Again Soon..");
+      }, 13700);
+      setTimeout(function () {
+        $("#prompt").html("See You Again Soon...");
+      }, 14000);
+    }
+  });
+  
